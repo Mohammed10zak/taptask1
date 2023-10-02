123965bc4a13364b80aeb210435406aaa763d93d
mohammed zakout <mohammedzakkh@gmail.com>
2023-10-02 22:40:30 +0300

    completing 2 tasks

---
+++ script.js
@@ -0,0 +1,19 @@
+function twoNums(nums, target) {
+  const numIndices = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    const nextNum = target - currentNum;
+
+    if (numIndices.hasOwnProperty(nextNum)) {
+      return [numIndices[nextNum], i];
+    }
+
+    numIndices[currentNum] = i;
+  }
+  return [];
+}
+const nums = [2, 7, 11, 15,17];
+const target = 9;
+const result = twoNums(nums, target);
+console.log(result);
